80d7e4ea47e9fd96af9fea8326f7db522dd759a0
Aaron <aaronphilipose@gmail.com>
2024-05-04 02:20:48 -0400

    Adding weduploader

--- src/App.vue
+++ src/App.vue
@@ -76,6 +76,7 @@ const loadIntroduction = async (index) => {
 <template>
     <div class="max-w-4xl mx-auto py-5">
       <input v-model="search" class="w-full rounded-lg flex mb-4" placeholder="Type Your Name Here">
+      <a href="https://weduploader.com/upload/PS8XpsktOLhehI2a" class="w-full flex text-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">UPLOAD PHOTOS HERE!</a>
     </div>
 
     <EasyDataTable
